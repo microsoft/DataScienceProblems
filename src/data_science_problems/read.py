51d95b9266a9510505071ccaf6e098c8906f8e28
sksq96 <shubhamchandel@nyu.edu>
2022-02-02 02:22:53 +0530

    Add License

--- src/data_science_problems/read.py
+++ src/data_science_problems/read.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 from pathlib import Path
 from tqdm import tqdm
 
--- src/data_science_problems/utils.py
+++ src/data_science_problems/utils.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import gzip
 import os
 import json
